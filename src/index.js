b04304f1333ee011494abbdebfaa0664d0617926
antmar0417 <tonyboy@live.se>
2022-04-29 13:34:36 +0200

    created  App.js

--- src/index.js
+++ src/index.js
@@ -1,47 +1,5 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import SeasonDisplay from './SeasonDisplay';
-import Spinner from './Spinner';
+import React from "react";
+import ReactDOM from "react-dom";
+import App from "./App";
 
-class App extends React.Component {
-    state = { lat: null, errorMessage: '' };
-
-    componentDidMount() {
-        window.navigator.geolocation.getCurrentPosition(
-            position => this.setState({ lat: position.coords.latitude, longtitude: position.coords.longitude }),
-            err => this.setState({ errorMessage: err.message })
-        );
-
-    }
-
-    componentDidUpdate() {
-        console.log('Updated')
-        // window.navigator.geolocation.getCurrentPosition(
-        //     position => console.log(position)
-        // );
-    }
-
-    renderContent () {
-        if (this.state.errorMessage && !this.state.lat) {
-            return <div>Error: {this.state.errorMessage}</div>
-        }
-        if (!this.state.errorMessage && this.state.lat) {
-            return <SeasonDisplay lat={this.state.lat} />
-        }
-        return <Spinner message="Pleace accept location request!" />;
-    }
-
-    render(){
-        return (
-            <div className="border-red">
-                {this.renderContent()}
-            </div>
-        );
-    }
-}
-
-ReactDOM.render(
-    <App />,
-    document.querySelector('#root')
-);
-
+ReactDOM.render(<App />, document.querySelector("#root"));
